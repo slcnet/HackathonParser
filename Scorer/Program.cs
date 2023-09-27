79dffb27c7f1907e6a5e505e3f2988d432b899cf
Kaleb Pederson <kaleb.pederson@gmail.com>
2023-09-26 21:35:59 -0600

    Add a scoring program. Treat test files as binary.

---
+++ Scorer/Program.cs
@@ -0,0 +1,21 @@
+﻿if (args.Length != 2)
+{
+    Console.WriteLine("Usage: Scorer <expectedFile> <actualFile>");
+    return;
+}
+
+var expectedLines = File.ReadAllLines(args[0]);
+var actualLines = File.ReadAllLines(args[1]);
+var score = 0;
+var i = 0;
+while (i < expectedLines.Length && i < actualLines.Length)
+{
+    // this is slightly generous compared to the requirements
+    var expected = expectedLines[i];
+    Console.WriteLine($"expected: '{expected}'");
+    var actual = actualLines[i];
+    score += expected.Equals(actual, StringComparison.InvariantCulture) ? 1 : 0;
+    i++;
+}
+
+Console.WriteLine(score);
